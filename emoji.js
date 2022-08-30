7222c0933eb1f6f2427bb80aede4b4b9d5915569
coolcjh <111398552+coolcjh@users.noreply.github.com>
2022-08-30 11:13:38 +0900

    Add files via upload

---
+++ emoji.js
@@ -0,0 +1,11 @@
+var testString = "🙈🙉🙊🦴👀👁👨‍🍳👩‍🍳👩‍💼🧞🧎‍♀🏌‍♀👨‍❤‍💋‍👨👩‍👦👫🤼🚵‍♀🧑‍🦽💇🤴1🥰🥵🥶🥳🥴🥺👨‍🦰👩‍🦰👨‍🦱👩‍🦱👨‍🦲👩‍🦲👨‍🦳👩‍🦳🎨🎦a😀😃😄😁c🖐✋🙅🙆‍♂👨‍👩‍👧👨‍👩‍👧‍👦👩‍👦b"; 
+var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+
+var strVal = testString.replace(regex,'');
+console.log("testString", testString);
+console.log("strVal", strVal);
+ 
+//var converted = strVal.replace(/[\u{1F004}-\u{1F9E6}]|[\u{1F600}-\u{1F9D0}]/gu, ""); 
+if(testString!=strVal){
+    alert("이모지는 사용할 수 없습니다.");
+}
